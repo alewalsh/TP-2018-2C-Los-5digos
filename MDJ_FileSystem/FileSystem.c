7927f5a2a7a8cf4a729088a9cc5e63075e25d07a
Diego Rodriguez Baumann <diegorodriguezbaumann@est.frba.utn.edu.ar>
2018-09-08 11:57:20 -0300

    Creo FS.

---
+++ MDJ_FileSystem/FileSystem.c
@@ -0,0 +1,5 @@
+#include "FileSystem.h"
+
+int main(){
+	return 0;
+}
---
+++ MDJ_FileSystem/FileSystem.h
@@ -0,0 +1,11 @@
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/log.h>
+#include <configuracion.h>
+#include <socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <unistd.h>
+//#include <pthread.h>
+#include <stdbool.h>
+#include <string.h>
