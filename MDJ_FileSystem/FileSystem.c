58581d6bfe398ff56fd4e7716c7c0c8f5020b64e
Diego Rodriguez Baumann <diegorodriguezbaumann@est.frba.utn.edu.ar>
2018-09-13 13:27:16 -0300

    avances en configuracion.

--- MDJ_FileSystem/FileSystem.c
+++ MDJ_FileSystem/FileSystem.c
@@ -1,6 +1,58 @@
 #include "FileSystem.h"
 
-int main(){
+int main(int argc, char ** argv) {
+	inicializarCPU(argv[1]);
+//	inicializarConexion();
 
-	return 0;
+	exit_gracefully(FIN_EXITOSO);
+
+	return 0; //TODO cambiar por exit gracefully
+}
+
+void inicializarCPU(char * pathConfig){
+	logger = log_create("MDJ.log", "MDJ", true, LOG_LEVEL_INFO);
+	if (pathConfig != NULL){
+		configuracion = cargarConfiguracion(pathConfig, MDJ, logger);
+	}
+	else{
+		log_error(logger, "No hay un path correcto a un archivo de configuracion");
+		exit_gracefully(ERROR_PATH_CONFIG);
+	}
+	if (configuracion == NULL){
+		log_error(logger, "Error en el archivo de configuracion");
+		exit_gracefully(ERROR_CONFIG);
+	}
+
+	//Imprimo lo leido en config.cfg
+	printf("Configuracion inicial: \n");
+	log_info(logger, "Puerto = %d", configuracion->puertoMDJ);
+	log_info(logger, "Punto montaje = %s", configuracion->puntoMontaje);
+	log_info(logger, "Retardo = %d", configuracion->retardo);
+}
+
+//void inicializarConexion(){
+//	int socketEscucha;
+//	escuchar(configuracion->puertoMDJ, socketEscucha, logger);
+//	esperarInstruccionDAM(socketEscucha);
+//}
+//
+//void esperarInstruccionDAM(int socketEscucha){
+//	while(1){
+//		int socketCliente;
+//		aceptar(socketEscucha, socketCliente, logger);
+//
+//		t_package instruccion;
+//		recibir(socketCliente, instruccion, logger);
+//
+//		//switch segun contenido de instruccion
+//	}
+//}
+
+void exit_gracefully(int error){
+	if (error != ERROR_PATH_CONFIG)
+	{
+		free(configuracion);
+	}
+	log_destroy(logger);
+	exit(error);
 }
--- MDJ_FileSystem/FileSystem.h
+++ MDJ_FileSystem/FileSystem.h
@@ -1,20 +1,31 @@
 #ifndef FileSystem_H_
 #define FileSystem_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <commons/config.h>
 #include <commons/string.h>
 #include <commons/log.h>
 #include <configuracion.h>
 #include <socket.h>
-#include <stdio.h>
-#include <stdlib.h>
-//#include <unistd.h>
-//#include <pthread.h>
-#include <stdbool.h>
-#include <string.h>
-
+#include <pthread.h>
 
 t_log* logger;
+configMDJ* configuracion;
 
+enum codigosError
+{
+	ERROR_SOCKET_DAM,
+	ERROR_PATH_CONFIG,
+	ERROR_CONFIG,
+	FIN_EXITOSO
+};
+
+void inicializarCPU(char *);
+void inicializarConexion();
+void esperarInstruccionDAM(int);
+void exit_gracefully(int);
 
 #endif /* FileSystem_H_ */
