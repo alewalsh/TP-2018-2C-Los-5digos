787c9cdc0cadc5ad65e9292d61df62a5746f2436
Nickingo <nico.barrelier@gmail.com>
2018-12-13 00:49:59 -0300

    Eliminando comentarios

--- SAFA/src/funcionesConsola.h
+++ SAFA/src/funcionesConsola.h
@@ -13,10 +13,7 @@
 #include <pthread.h>
 #include "funcionesSAFA.h"
 #include <semaphore.h>
-//#include "handlerConnections.h"
 
-
-extern int shouldExit;
 extern pthread_mutex_t mutexExit;
 
 extern t_list* colaNew;			// Lista New.
@@ -28,66 +25,19 @@ extern t_list* colaExit;		// Lista Terminados.
 
 extern sem_t mandadosPorConsola;
 
-//extern pthread_mutex_t mutexReadyExecute;
-//
-//extern sem_t sem_newEsi;
-//extern int coordinador;
-//
-
-
-
-
 void consolaEjecutar(char *args);
 void consolaLiberar();
-
 void consolaStatus();
 void imprimirNEW();
 void imprimirREADY();
 void imprimirEJECUTANDO();
-
 void consolaMetricasDTB(char *args);
 void consolaMetricas();
-
-//void consoleBlock(char *args);
-//
-//void consoleList(char *key);
-//
-//void consoleKill(char *id);
-//
-//void consoleDeadlock();
-//
-//void consolePrintMan();
-//
-//void consoleStop();
-
-
 void consoleExit();
-
 void consoleHelp();
-
-//void consoleStatusAllESI();
-
 void consoleClear();
-
-//char *statusToString(uint16_t status);
-//
-//char *statusKeyToSting(uint16_t status);
-//
-//void consoleStatusKeys();
-//
-//void detectDeadlock(int e, int k, int need[e][k], int alloc[e][k], int avail[k], t_list *esis);
-//
-
 int getIdFunction(char *function);
 void parseCommand(char *line, char **command, char **args);
 void freeCommand(char *command, char *args);
-
-//--------------------------------------------------------------------------------------------------------------------------------------
-// FUNCIONES PARA LOS HILOS
-//--------------------------------------------------------------------------------------------------------------------------------------
-int getExit();
-void setExit();
-
-
 
 #endif /* FUNCIONESCONSOLA_H_ */
