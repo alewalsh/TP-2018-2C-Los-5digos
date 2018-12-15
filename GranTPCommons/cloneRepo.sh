435be31c3e196492c7e7acd932d37cf1d8ff1c11
Nickingo <nico.barrelier@gmail.com>
2018-12-15 05:26:22 -0300

    Agrego archivos bash para el deploy

---
+++ GranTPCommons/cloneRepo.sh
@@ -0,0 +1,7 @@
+mkdir prueba
+cd prueba
+git clone https://github.com/sisoputnfrba/tp-2018-2c-Los-5digos.git
+cd tp-2018-2c-Los-5digos
+git checkout ramaFinal
+cd ..
+cd ..
---
+++ GranTPCommons/funesmemory9.sh
@@ -0,0 +1,4 @@
+cd Git/tp-2018-2c-Los-5digos/FunesMemory9/Debug
+make clean
+make all
+./FunesMemory9 ../config.cfg
